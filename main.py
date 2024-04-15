474e51c80d8deff6dcf2dae78d0b10d753b9caa7
Sammy Medawar <medawarsammy@gmail.com>
2024-04-15 19:16:50 +0300

    3D Model API Upload

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
-from DlibActuator import DlibActuator
+from fastapi.responses import FileResponse
+#from DlibActuator import DlibActuator
+from ModelCreator import ModelCreator
+import base64
+from pydantic import BaseModel
 
 app = FastAPI()
-dlib = DlibActuator()
+#dlib = DlibActuator()
+modelCreator = ModelCreator()
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
     "*"
 ]
+
+class FileInput(BaseModel):
+    file_name: str
+    file_data: str
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,17 +29,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# @app.post("/process_images")
+# def process_images(files: List[UploadFile] = File(...)):
+#     image_paths = []
+#     for file in files:
+#         with open(f"./uploads/{file.filename}", "wb") as buffer:
+#             buffer.write(file.file.read())
+#         image_paths.append(f"./uploads/{file.filename}")
+
+#     result = dlib.process_images(image_paths)
+
+#     return result
+
+# @app.post("/process_images")
+# def process_images(files: List[UploadFile] = File(...)):
+#     image_paths = []
+#     for file in files:
+#         with open(f"./uploads/{file.filename}", "wb") as buffer:
+#             buffer.write(file.file.read())
+#         image_paths.append(f"./uploads/{file.filename}")
+
+#     result_path = modelCreator.create_model(image_paths)
+
+#     return FileResponse(result_path, media_type="application/octet-stream")
+
 @app.post("/process_images")
-def process_images(files: List[UploadFile] = File(...)):
+def process_images(files: List[UploadFile]):
     image_paths = []
     for file in files:
         with open(f"./uploads/{file.filename}", "wb") as buffer:
             buffer.write(file.file.read())
         image_paths.append(f"./uploads/{file.filename}")
 
-    result = dlib.process_images(image_paths)
+    result_path = modelCreator.create_model(image_paths)
 
-    return result
+    return FileResponse(result_path, media_type="application/octet-stream")
 
 @app.get("/check_api")
 def check_api():
